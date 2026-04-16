efdb12f79a0b2b96b5b6f9860fd0442f9a81ebeb
emirdelipinar-dot <emirdelipinar@gmail.com>
2026-04-16 20:19:53 +0100

    Starting from Shape.java

--- src/Shape.java
+++ src/Shape.java
@@ -1,2 +1,11 @@
-public class Shape {
+public abstract class Shape {
+    protected Coordinates position;
+    protected int sides;
+
+    public Shape(int noOfSides, Coordinates coord) {
+        this.position = coord;
+        this.sides = noOfSides;
+    }
+
+
 }
