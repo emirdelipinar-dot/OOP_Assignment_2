3664a05c83e477eadc6ff29bb66c859bec74e015
emirdelipinar-dot <emirdelipinar@gmail.com>
2026-04-16 17:34:02 +0100

    Adding the all needed classes and writing the main_menu code to Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,68 @@
-public class Main {
+import java.util.Scanner;
 
-    public class Main {
-        public static void main(String[] args) {
-            System.out.println("***** Shape Management Application ******");
-            //Test coordinates class
-            Coordinates point1 = new Coordinates(10, 20);
-            System.out.println(point1.display());
-            //Test Rectangle class
-            Rectangle r1 = new Rectangle(point1, 10, 20);
-            Coordinates pointA = new Coordinates(2, 3);
-            double d = point1.distance(pointA);
-            System.out.println("Distance between point 1 and point A " + d);
-            System.out.println(r1.display());
-            //Test Circle class
+public class Main {
+    public static void main(String[] args) {
+
+        //Create Scanner
+        Scanner input = new Scanner(System.in);
+        //String Menu
+        String menu = "\n ***** Shape Management Application ****** \n";
+        menu += "Menu\n";
+        menu += "1- Add Shape\n";
+        menu += "2- Remove Shape\n";
+        menu += "3- Show all shapes\n";
+        menu += "4- Exit\n";
+        menu += "Enter your Choice (1..4): ";
+
+        //Repeat printing menu and get the user option
+        while(true){
+            System.out.print(menu);
+            char choice = input.next().charAt(0);//get the user choice
+            input.nextLine();//to consume the leftover newline character (\n) from the input buffer.
+            if (choice == '1'){
+                System.out.print("Enter shape type to add: (C , T, S, R) ");
+                char type = input.next().toUpperCase().charAt(0);
+                if(type == 'C'){
+                    //add your code here
+                    System.out.println("A new Circle created.");
+                }
+                else if(type == 'T'){
+                    //add your code here
+                    System.out.println("A new Triangle created.");
+                }
+                else if(type == 'S'){
+                    //add your code here
+                    System.out.println("A new Square created.");
+                }
+                else if(type == 'R'){
+                    //add your code here
+                    System.out.println("A new Rectangle created.");
+                }
+                else{
+                    System.out.println("Invalid input, Try again.");
+                }
+            }
+            else if (choice == '2'){
+                System.out.print("Enter shape ID to remove: ");
+                int id = input.nextInt();
+                // add you code here
+                System.out.println("\nA shape has been Successfully removed.");
+            }
+            else if (choice == '3'){
+                System.out.println("\n-----List of all Shapes----\n ");
+                // add you code here
+            }
+            else if (choice == '4'){
+                System.out.println("Exiting .....");
+                break;
+            }
+            else{
+                System.out.println("Invalid input, Try again.");
+            }
         }
     }
-
-
-
-
 }
+
+
+
+
