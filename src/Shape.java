d2464d43e91346aec2d4465036b21e7acbfbf11b
emirdelipinar-dot <emirdelipinar@gmail.com>
2026-04-16 20:43:48 +0100

    Shape.java is finished for now.

--- src/Shape.java
+++ src/Shape.java
@@ -6,6 +6,18 @@ public abstract class Shape {
         this.position = coord;
         this.sides = noOfSides;
     }
+    public Coordinates getCoordinates() { return position; }
+    public int getSides() { return sides; }
+    public void setCoordinates(Coordinates newCoord) { this.position = newCoord; }
 
+    public void translate(int dx, int dy) {
+        position.translate(dx, dy);
+    }
+
+    public abstract void scale(int factor, boolean sign);
+    public abstract double getArea();
+    public abstract double getPerimeter();
+    public abstract String display();
+}
 
 }
