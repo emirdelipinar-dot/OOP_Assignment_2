0f15ed9bf1359975decf102556cdc798b3578c4a
emirdelipinar-dot <emirdelipinar@gmail.com>
2026-04-17 22:12:17 +0100

    Creating a new class called Square and starting to write code.

--- src/Square.java
+++ src/Square.java
@@ -1,4 +1,16 @@
-public class Square {
+public class Square extends Shape {
+    private double side;
+
+    public Square(Coordinates coord, double side) {
+        super(4, coord);
+        this.side = side;
+    }
+
+    @Override
+    public void scale(int factor, boolean sign) {
+        if (sign) side *= factor;
+        else side /= factor;
+    }
 
 
 
