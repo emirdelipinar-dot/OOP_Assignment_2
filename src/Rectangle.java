17b34d7c9d4c56f219bbb68034e4a8671ef152f1
emirdelipinar-dot <emirdelipinar@gmail.com>
2026-04-17 16:11:55 +0100

    Circle.java is finished , starting to Rectangle class.

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,2 +1,21 @@
-public class Rectangle {
+public class Rectangle extends Shape {
+    private double width;
+    private double length;
+
+    public Rectangle(Coordinates coord, double width, double length) {
+        super(4, coord);
+        this.width = width;
+        this.length = length;
+    }
+
+    @Override
+    public void scale(int factor, boolean sign) {
+        if (sign) {
+            width *= factor;
+            length *= factor;
+        } else {
+            width /= factor;
+            length /= factor;
+        }
+    }
 }
