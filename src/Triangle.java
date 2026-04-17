7bda19174c99885d21b674df89b5b2e35af78626
emirdelipinar-dot <emirdelipinar@gmail.com>
2026-04-18 00:56:38 +0100

    Triangle.java is finished moving to ShapeList

--- src/Triangle.java
+++ src/Triangle.java
@@ -21,4 +21,25 @@ public class Triangle extends Shape {
         p2.scale(factor, sign);
         p3.scale(factor, sign);
     }
+    @Override
+    public double getArea() {
+        double a = position.distance(p2);
+        double b = p2.distance(p3);
+        double c = p3.distance(position);
+        double s = (a + b + c) / 2.0;
+        return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+
+    @Override
+    public double getPerimeter() {
+        return position.distance(p2) + p2.distance(p3) + p3.distance(position);
+    }
+
+    @Override
+    public String display() {
+        return "Triangle [P1: " + position.display() + ", P2: " + p2.display() +
+                ", P3: " + p3.display() + ", Area: " + String.format("%.2f", getArea()) + "]";
+    }
+
+
 }
