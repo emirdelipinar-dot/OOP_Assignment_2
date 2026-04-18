e54d1d301e1467f0e8cc531f691eb8602d01cbcc
emirdelipinar-dot <emirdelipinar@gmail.com>
2026-04-18 13:13:14 +0100

    Using ArrayList library.

--- src/ShapeList.java
+++ src/ShapeList.java
@@ -1,4 +1,22 @@
-public class ShapeList {
+import java.util.ArrayList;
 
+public class ShapeList {
+    private ArrayList<Shape> listOfShapes = new ArrayList<>();
+
+    public void addShape(Shape s) { listOfShapes.add(s); }
+
+    public void removeShape(int pos) {
+        if (pos > 0 && pos <= listOfShapes.size()) {
+            listOfShapes.remove(pos - 1);
+        }
+    }
+
+    public void translateShapes(int dx, int dy) {
+        for (Shape s : listOfShapes) s.translate(dx, dy);
+    }
+
+    public void scaleShapes(int factor, boolean sign) {
+        for (Shape s : listOfShapes) s.scale(factor, sign);
+    }
 
 }
