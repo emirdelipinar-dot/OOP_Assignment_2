795597a5f356c7e6d340a1b19cdb407130688b51
emirdelipinar-dot <emirdelipinar@gmail.com>
2026-04-18 16:05:25 +0100

    Fixed some issues.

--- src/Coordinates.java
+++ src/Coordinates.java
@@ -2,43 +2,36 @@ public class Coordinates {
     private int x;
     private int y;
 
-    public Coordinates(int x1, int y1){
-        this.x = x1;
-        this.y = y1;
-
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
-public int getX() { return x; }
-    public int getY() { return y;}
+
+    public int getX() { return x; }
+    public int getY() { return y; }
 
     public double distance(Coordinates p) {
         int distX = this.x - p.getX();
         int distY = this.y - p.getY();
         return Math.sqrt(distX * distX + distY * distY);
     }
-        public void translate(int dx, int dy){
-            this.x += dx;
-            this.y += dy;
+
+    public void translate(int dx, int dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+
+    public void scale(int factor, boolean sign) {
+        if (sign) {
+            this.x *= factor;
+            this.y *= factor;
+        } else {
+            this.x /= factor;
+            this.y /= factor;
         }
-   public void scale(int factor, boolean sign){
-            if(sign){
-                this.x *= factor;
-                this.y *= factor;
-            }
-            else{
-                this.x /= factor;
-                this.y /= factor;
-            }
+    }
 
-        }
-   public String display() {
-            String msg = "X = "+ x +",Y =" + y;
-            if(x < 0 || y < 0){
-                msg += "\n Note: Negative X or Y,Coordinates are out of zone.\n";
-            }
-      return msg;
-        }
-
-
-
-
+    public String display() {
+        return "X = " + x + ", Y = " + y;
+    }
 }
--- src/Triangle.java
+++ src/Triangle.java
@@ -3,7 +3,7 @@ public class Triangle extends Shape {
     private Coordinates p3;
 
     public Triangle(Coordinates p1, Coordinates p2, Coordinates p3) {
-        super(3, p1); // position p1'i temsil eder
+        super(3, p1);
         this.p2 = p2;
         this.p3 = p3;
     }
