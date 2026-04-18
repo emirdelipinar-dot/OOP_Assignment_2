6349b26cb0ec48b9e03f28a2f3ce9bf91ea1c424
emirdelipinar-dot <emirdelipinar@gmail.com>
2026-04-18 21:51:05 +0100

    entering the square values in a rectangle shape problem has been solved.

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -2,34 +2,44 @@ public class Rectangle extends Shape {
     private double width;
     private double length;
 
-    public Rectangle(Coordinates coord, double width, double length) {
+    /* Constructor with validation to ensure it is not a square */
+    public Rectangle(Coordinates coord, double width, double length) throws Exception {
         super(4, coord);
+        if (width == length) {
+            throw new Exception("Geometrical Error: Width and length cannot be equal. For equal sides, please create a Square.");
+        }
+        if (width <= 0 || length <= 0) {
+            throw new Exception("Geometrical Error: Dimensions must be positive values.");
+        }
         this.width = width;
         this.length = length;
     }
 
     @Override
-    public void scale(int factor, boolean sign) {
-        if (sign) {
-            width *= factor;
-            length *= factor;
-        } else {
-            width /= factor;
-            length /= factor;
-        }
+    public double getArea() {
+        return width * length;
     }
-    @Override
-    public double getArea() { return width * length; }
 
     @Override
-    public double getPerimeter() { return 2 * (width + length); }
+    public double getPerimeter() {
+        return 2 * (width + length);
+    }
+
+    @Override
+    public void scale(int factor, boolean sign) {
+        if (sign) {
+            this.width *= factor;
+            this.length *= factor;
+        } else {
+            if (factor != 0) {
+                this.width /= factor;
+                this.length /= factor;
+            }
+        }
+    }
 
     @Override
     public String display() {
-        return "Rectangle [Width: " + width + ", Length: " + length +
-                ", Pos: " + position.display() + ", Area: " + getArea() + "]";
+        return "Rectangle [Width: " + width + ", Length: " + length + ", Position: " + position.display() + "]";
     }
-
-
-
 }
